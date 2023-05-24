21c792e7539493c32cce43b6028a7c0328d6992d
noor-ain <checkmate9080@gmail.com>
2023-05-24 12:09:40 +0500

    added files working

--- removebg.py
+++ removebg.py
@@ -1,9 +1,8 @@
 from rembg import remove
 from PIL import Image
 
-input_path = 'picture.png'
 output_path = 'rembg.png'
-input = Image.open(input_path)
+input = Image.open("removebg.jpg")
 output = remove(input)
 output.save(output_path)
 
